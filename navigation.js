096b810cffc9e00eedd0cd05f17825037886ce20
prajwal799 <prajwalgoyal1999@gmail.com>
2021-10-10 21:46:02 +0530

    search any meal

---
+++ navigation.js
@@ -0,0 +1,13 @@
+function Navbar(){
+    return `<div id="nav">
+    <ul>
+    <li><a href="food1.html">recipes</a></li>
+    <li><a href="food2.html">random</a></li>
+    <li><a href="food3.html">latest</a></li>
+</ul>
+    </div>`
+}
+
+
+
+export default Navbar;
