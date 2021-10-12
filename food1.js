42d963da5fb22993dec0bbe7b0642fa70b89640a
prajwal799 <prajwalgoyal1999@gmail.com>
2021-10-12 23:13:10 +0530

    new files

--- food1.js
+++ food1.js
@@ -1,37 +1,11 @@
-import Navbar from "./navigation.js";
+import Navbar , {fetchdata} from "./navigation.js";
 
 const navbar = Navbar();
 const container = document.getElementById('nav');
 container.innerHTML = navbar;
 
-const input = document.getElementById('search').value;
-const btn = document.getElementById('btn');
+const fetchData = fetchdata();
+const data = document.getElementById("recipes");
+data.innerHTML = fetchData;
 
-btn.addEventListener("click",search);
 
-async function search(){
-  const input = document.getElementById('search').value;
-   const data = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`)
-
-   const list = await data.json();
-
-   const meal = await list.meals;
-   
-   const recipes = document.getElementById("recipes");
-   recipes.textContent = "";
-   for(let userData of meal){
-    const div = document.createElement("div");
-    const h1 = document.createElement("h1");
-    h1.textContent = userData.strMeal;
-
-    const img = document.createElement('img');
-    img.src = userData.strMealThumb;
-    img.style.width = "200px";
-    img.style.height = "200px";
-
-    div.append(h1,img);
-    recipes.append(div);
- }    
-   
-   
-}
--- navigation.js
+++ navigation.js
@@ -8,6 +8,43 @@ function Navbar(){
     </div>`
 }
 
+ 
+ export function fetchdata(){
+    return `<div id="recipes>
+    const input = document.getElementById('search').value;
+const btn = document.getElementById('btn');
+
+btn.addEventListener("click",search);
+search();
+
+async function search(){
+  const input = document.getElementById('search').value;
+   const data = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=pizza")
+
+   const list = await data.json();
+
+   const meal = await list.meals;
+   const recipes = document.getElementById("recipes");
+   recipes.textContent = "";
+   for(let userData of meal){
+    const div = document.createElement("div");
+    const h1 = document.createElement("h1");
+    h1.textContent = userData.strMeal;
+
+    const img = document.createElement('img');
+    img.src = userData.strMealThumb;
+    img.style.width = "200px";
+    img.style.height = "200px";
+
+    div.append(h1,img);
+    recipes.append(div);
+ }    
+   
+   
+}
+             </div>`
+}
+
 
 
 export default Navbar;
