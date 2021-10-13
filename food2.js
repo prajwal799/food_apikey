1e2ab6e5ff9a26e7860d4eff08ac3f7955b0fdb1
prajwal799 <prajwalgoyal1999@gmail.com>
2021-10-13 16:11:04 +0530

    change in food2 files

--- food2.js
+++ food2.js
@@ -1,37 +1,16 @@
-import Navbar from "./navigation.js";
+import {Navbar,getdata,fetchdata} from "./navigation.js";
 
 const navbar = Navbar();
 const container = document.getElementById("nav");
 container.innerHTML = navbar;
 
 
-async function random(){
-     const data = await fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
-  
-     const list = await data.json();
-  
-     const meal = await list.meals;
-     console.log(meal,"melas")
-     
-     const recipes = document.getElementById("recipes");
-     for(let userData of meal){
-       const div = document.createElement("div");
-       const h1 = document.createElement("h1");
-       h1.textContent = userData.strMeal;
-  
-       const img = document.createElement('img');
-       img.src = userData.strMealThumb;
-       img.style.width = "200px";
-       img.style.height = "200px";
+const data = document.getElementById("recipes");
 
-       const p =document.createElement('p');
-       p.textContent = userData.strInstructions;
-  
-       div.append(h1,img,p);
-        recipes.append(div);
-    }
+let listData = getdata(`https://www.themealdb.com/api/json/v1/1/random.php`)
+listData.then( (res) => {
+  fetchdata(res , data);
+})
     
       
-  }
-
-random()
+  
